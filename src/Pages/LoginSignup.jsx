49bfab4910e649d99e9e38a1af810954cb8fe670
nguyetanhdam <20020129@vnu.edu.vn>
2023-12-13 20:02:42 +0700

    Login and Signup page

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './CSS/LoginSignup.css';
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+      <div className='loginsignup-container'>
+        <h1>Sign Up</h1>
+        <div className='loginsignup-fields'>
+          <input type='text' placeholder='Your name' />
+          <input type='email' placeholder='Email address' />
+          <input type='password' placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>Already have an account? <span>Login here</span></p>
+        <div className='loginsignup-agree'>
+          <input type='checkbox' name='' id='' />
+          <p>By continuing, I agree to your Terms of Service & Privacy Policy</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
